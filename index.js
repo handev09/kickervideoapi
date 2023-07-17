3b4db69d7476b15a482bd1525752208b8bc2f954
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-07-17 21:50:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,12 +33,7 @@ app.post('/api/v1/register', (req, res) => {
         console.error('Error during registration:', error);
         res.status(500).json({ error: 'Registration failed' });
       } else {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-        res.setHeader(
-          'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        );
+    
         res.status(200).json({
           message: 'Registration successful',
           redirectUrl: `/dashboard?userId=${userId}&name=${encodeURIComponent(name)}`,
