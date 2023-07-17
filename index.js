baa04dce2541d2ac4101b54d089704e1af2a9b83
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-07-17 18:07:37 +0300

    Initial api setup
    This is the initial api i setup for testing purposes

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  host: 'kickervideo-db.cpp0rtsplxx9.eu-north-1.rds.amazonaws.com',
+  user: 'hankis',
+  password: 'ESYN1dqjY6mIp3kOep8y',
+  database: 'kickervideo',
+  connectionLimit: 10,
+});
+
+// Handle registration request
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Insert user data into the database
+  pool.query(
+    'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+    [name, email, password],
+    (error, results) => {
+      if (error) {
+        console.error('Error during registration:', error);
+        res.status(500).json({ error: 'Registration failed' });
+      } else {
+        // res.status(200).json({ message: 'Registration successful' });
+        const userId = results.insertId;
+        res.status(200).json({ userId, name, message: 'Registration successful' });
+      }
+    }
+  );
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
