be2d103693ec611f49cb0a239f8c09b47e59849f
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-07-17 21:42:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
