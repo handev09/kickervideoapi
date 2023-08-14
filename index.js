63274819c20471bb9cc53f217c6e20453e33cf63
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-08-14 18:34:03 +0300

    changed localhost listening port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
