dbfc2a0eae81769245d32710c8d0bc95ed6c7682
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2023-08-15 05:48:45 +0300

    Added hashing for the passwords
    In this commit i have added a basic bycrypt hashing for the user's passwords

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,5 +1,62 @@
+// const express = require('express');
+// const mysql = require('mysql2');
+// const router = express.Router();
+// const bcrypt = require('bcryptjs');
+
+
+
+// // Handle registration request
+// router.post('/', async(req, res) => {
+//   const { name, email, password } = req.body;
+
+//   // Check if the user with the given email already exists
+//   pool.query(
+//     'SELECT * FROM users WHERE email = ?',
+//     [email],
+//     (error, results) => {
+//       if (error) {
+//         console.error('Error during registration:', error);
+//         res.status(500).json({ error: 'Registration failed' });
+//       } else {
+//         if (results.length > 0) {
+//           res.status(409).json({ error: 'User with this email already exists' });
+//         } else {
+//           // Hash the password
+//       const hashedPassword = await bcrypt.hash(password, 10); // Hash the password with a salt of 10 rounds
+//           // Insert user data into the database
+//           pool.query(
+//             'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+//             [name, email, password],
+//             (error, results) => {
+//               if (error) {
+//                 console.error('Error during registration:', error);
+//                 res.status(500).json({ error: 'Registration failed' });
+//               } else {
+//                 if (results && results.insertId) {
+//                   const userId = results.insertId;
+//                   res.status(200).json({
+//                     message: 'Registration successful',
+//                     userId: userId,
+//                     name: name,
+//                   });
+//                 } else {
+//                   console.error('No insertId found in the database results:', results);
+//                   res.status(500).json({ error: 'Registration failed' });
+//                 }
+//               }
+//             }
+//           );
+//         }
+//       }
+//     }
+//   );
+// });
+
+// module.exports = router;
+
 const express = require('express');
 const mysql = require('mysql2');
+const bcrypt = require('bcryptjs'); // Import bcryptjs
 const router = express.Router();
 
 // Create a MySQL connection pool
@@ -10,50 +67,56 @@ const pool = mysql.createPool({
   database: 'kickervideo',
   connectionLimit: 10,
 });
-
 // Handle registration request
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { name, email, password } = req.body;
 
-  // Check if the user with the given email already exists
-  pool.query(
-    'SELECT * FROM users WHERE email = ?',
-    [email],
-    (error, results) => {
-      if (error) {
-        console.error('Error during registration:', error);
-        res.status(500).json({ error: 'Registration failed' });
-      } else {
-        if (results.length > 0) {
-          res.status(409).json({ error: 'User with this email already exists' });
+  try {
+    // Check if the user with the given email already exists
+    const emailExists = await new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
+        if (error) {
+          reject(error);
         } else {
-          // Insert user data into the database
-          pool.query(
-            'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-            [name, email, password],
-            (error, results) => {
-              if (error) {
-                console.error('Error during registration:', error);
-                res.status(500).json({ error: 'Registration failed' });
-              } else {
-                if (results && results.insertId) {
-                  const userId = results.insertId;
-                  res.status(200).json({
-                    message: 'Registration successful',
-                    userId: userId,
-                    name: name,
-                  });
-                } else {
-                  console.error('No insertId found in the database results:', results);
-                  res.status(500).json({ error: 'Registration failed' });
-                }
-              }
-            }
-          );
+          resolve(results.length > 0);
         }
-      }
+      });
+    });
+
+    if (emailExists) {
+      res.status(409).json({ error: 'User with this email already exists' });
+    } else {
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10); // Hash the password with a salt of 10 rounds
+
+      // Insert user data into the database
+      pool.query(
+        'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+        [name, email, hashedPassword], // Use the hashed password
+        (error, results) => {
+          if (error) {
+            console.error('Error during registration:', error);
+            res.status(500).json({ error: 'Registration failed' });
+          } else {
+            if (results && results.insertId) {
+              const userId = results.insertId;
+              res.status(200).json({
+                message: 'Registration successful',
+                userId: userId,
+                name: name,
+              });
+            } else {
+              console.error('No insertId found in the database results:', results);
+              res.status(500).json({ error: 'Registration failed' });
+            }
+          }
+        }
+      );
     }
-  );
+  } catch (error) {
+    console.error('Error during registration:', error);
+    res.status(500).json({ error: 'Registration failed' });
+  }
 });
 
 module.exports = router;
