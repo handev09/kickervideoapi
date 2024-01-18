dc9cdf0cf67233a6be356a44f1416d68d6edd916
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2024-01-18 19:55:01 +0300

    Update update.js

--- routes/budget/update.js
+++ routes/budget/update.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
 const router = express.Router();
-const mysql = require("mysql2");
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
@@ -10,35 +10,103 @@ const pool = mysql.createPool({
   database: "kickervideo",
   connectionLimit: 10,
 });
-// Handle budget edit request
+
+// Handle budget update request
 router.put("/:budgetId", async (req, res) => {
   const budgetId = req.params.budgetId;
   const {
-    project_title,
-
-    internal_notes,
-
+    client,
+    projectTitle,
+    subtotal,
+    total,
+    internalNotes,
+    userId,
     status,
+    attachmentsUrl,
+    budgetNumber,
+    clientName,
+    serviceData,
+    tax,
+    discount,
   } = req.body;
 
   try {
     // Update budget data in the database
-    pool.query(
-      "UPDATE budgets SET budget_name=?,project_title=?,internal_notes=?, status=? WHERE budget_id=?",
-      [project_title, project_title, internal_notes, status, budgetId],
-      async (error, results) => {
-        if (error) {
-          console.error("Error during budget update:", error);
-          res.status(500).json({ error: "Budget update failed" });
-        } else {
-          res.status(200).json({
-            message: "Budget update successful",
-            budgetId: budgetId,
-        
-          });
-        }
-      }
+    await pool.query(
+      "UPDATE budgets SET budget_name=?, created_at=?, client_name=?, project_title=?, subtotal=?, total=?, internal_notes=?, user_id=?, status=?, attachments=?, budget_numb=?, company_client=?, tax=?, discount=? WHERE budget_id=?",
+      [
+        projectTitle,
+        new Date(),
+        clientName,
+        projectTitle,
+        subtotal,
+        total,
+        internalNotes,
+        userId,
+        status,
+        attachmentsUrl,
+        budgetNumber,
+        clientName,
+        tax,
+        discount,
+        budgetId,
+      ]
     );
+
+    // Update existing services and insert new services into the services table
+    for (const service of serviceData) {
+      console.log(service.id)
+      const [selectResults] = await pool.query(
+        "SELECT * FROM budgetItems WHERE item_id=?",
+        [service.id]
+      );
+      console.log(selectResults.length)
+
+      if (selectResults.length > 0) {
+        // If the item already exists, update it
+        await pool.query(
+          "UPDATE budgetItems SET item_name=?, budget_id=?, item_description=?, item_rate=?, item_cost=?, item_markup=?, item_unitPrice=?, item_quantity=?, user_id=?, created_at=? WHERE item_id=?",
+          [
+            service.name,
+            service.budgetId,
+            service.description,
+            service.item_rate,
+            service.cost,
+            service.markup,
+            service.unitPrice,
+            service.quantity,
+            service.userId,
+            service.createdAt,
+            service.id,
+          ]
+        );
+      } else {
+        // If the item doesn't exist, insert a new one
+        console.log("Executing else Block");
+        await pool.query(
+          "INSERT INTO budgetItems (item_id, item_name, budget_id, item_description, item_rate, item_cost, item_markup, item_unitPrice, item_quantity, user_id, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          [
+            service.id,
+            service.name,
+            service.budgetId,
+            service.description,
+            service.item_rate,
+            service.cost,
+            service.markup,
+            service.unitPrice,
+            service.quantity,
+            service.userId,
+            service.createdAt,
+          ]
+        );
+      }
+    }
+
+    res.status(200).json({
+      message: "Budget and items update successful",
+      budgetId: budgetId,
+      client: client,
+    });
   } catch (error) {
     console.error("Error during budget update:", error);
     res.status(500).json({ error: "Budget update failed" });
