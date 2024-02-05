2db05b441e12bbbf7509d76fb05d97c4fb71bc2e
Kisaka Handhara <123075744+handev09@users.noreply.github.com>
2024-02-05 14:46:21 +0300

    fix #01
    Added delete budget items route

---
+++ routes/budget-items/delete.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql2");
+
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  host: "kickervideo-db.cpp0rtsplxx9.eu-north-1.rds.amazonaws.com",
+  user: "hankis",
+  password: "ESYN1dqjY6mIp3kOep8y",
+  database: "kickervideo",
+  connectionLimit: 10,
+});
+
+// Delete Expense
+router.delete("/:itemId", async (req, res) => {
+  const itemId = req.params.itemId;
+
+  try {
+    // Delete expense with the specified ID from the database
+    const result = await pool
+      .promise()
+      .query("DELETE FROM budgetItems WHERE item_id = ?", [itemId]);
+
+    if (result[0].affectedRows > 0) {
+      res.status(200).json({
+        message: "Item deleted successfully",
+      });
+    } else {
+      res.status(404).json({ error: "Item not found" });
+    }
+  } catch (error) {
+    console.error("Error during Item deletion:", error);
+    res.status(500).json({ error: "Item deletion failed" });
+  }
+});
+
+module.exports = router;
--- routes/budget-items/index.js
+++ routes/budget-items/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 // const createContactsRoute = require('./create');
 const fetchBudgetItemsRoute = require('./fetch');
 // const fetchContactDetailsRoute = require('./fetchContact');
-// const deleteClientsRoute = require('./delete');
+const deleteBudgetItemsRoute = require('./delete');
 // const loginRoute = require('./login');
 
 // router.use('/create', createContactsRoute);
 router.use('/fetch', fetchBudgetItemsRoute);
 // router.use('/contact-details', fetchContactDetailsRoute);
-// router.use('/delete', deleteExpenseRoute);
+router.use('/delete', deleteBudgetItemsRoute);
 // router.use('/login', loginRoute);
 
 module.exports = router;
